dcc2b1a601a23e1afae94012aa9c9454eb5d04bf
gulcearda <gulceardaa@gmail.com>
2021-01-17 17:08:54 +0300

    test commit

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -6,7 +6,7 @@ namespace DegerVeReferansTipler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! test");
 
             int sayi1 = 10;
             int sayi2 = 30;
